6afb9a57473069a7137d7cc26e06d9ae8c6a786d
gowtham <gowthamsjbit@gmail.com>
2023-12-18 15:24:17 +0530

    create sign in api route

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -43,7 +43,6 @@ export default function SignUp() {
     }
   }
 
-
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign UP</h1>
